450f28aa47fbcdcc5ac4cd7176e62ee023b74c8a
Elizabeth Lotto <elizlotto@gmail.com>
2020-10-24 22:16:06 -0400

    switching branches

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-const MapContainer = require('./MapContainer');
+
 
 function App() {
   return (
     <div className='App'>
       <h1>hello world</h1>
-      <MapContainer />
+
     </div>
   );
 }
