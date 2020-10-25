cb5a5db0b0d80f398ceb485423544cd06be60dc9
Elizabeth Lotto <elizlotto@gmail.com>
2020-10-24 20:34:26 -0400

    went back to master version

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+const MapContainer = require('./MapContainer');
 
 function App() {
   return (
     <div className='App'>
-        <h1>Welcome to
-          <br/>Fullstack Webpack!</h1>
-        <div className='description'>
-          <h4>Visit us at</h4>
-          <h3>https://github.com/Create-Fullstack-Webpack/create-fullstack-webpack-app</h3>
-        </div>
+      <h1>hello world</h1>
+      <MapContainer />
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     path: path.resolve(process.cwd(), 'dist'),
     filename: 'bundle.js',
   },
+  target: 'node',
   devServer: {
     port: 8080,
     proxy: {
@@ -19,38 +20,41 @@ module.exports = {
   entry: './client/src/index.js',
   module: {
     rules: [
-    {
-      test: /.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react', '@babel/preset-env'],
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
         },
       },
-    },
-    {
-      test: /.(css|scss)$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    },
-    {
-      test: /.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
-      use: [
-        'file-loader',
-      ],
-    },
-  ]},
-resolve: {
-	extensions: ['.js', '.jsx', ]},
+      {
+        test: /.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx',]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './client/src/index.html',
     }),
-    new CleanWebpackPlugin(), 
-  new MiniCssExtractPlugin(), 
-]
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ]
 }
+
